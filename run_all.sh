b218245f9d1519c9c575a76d1770aa3f04966921
Sam <padilla.samuelk@gmail.com>
2023-04-08 16:53:13 +0000

    Updates to runner script

--- run_all.sh
+++ run_all.sh
@@ -1,5 +1,5 @@
 python3 main.py --blockchain=avalanche --providers=gcp,aws,htz,ovh --countries=us,de,gb,jp,cn,ca,au
-python3 main.py --blockchain=cardano --providers=gcp,aws,htz,ovh --countries=us,de,gb,jp,cn,ca,au
+python3 main.py --blockchain=cardano --providers=gcp,aws,htz,ovh,doc,ctb --countries=us,de,gb,jp,cn,ca,au
 python3 main.py --blockchain=solana --providers=gcp,aws,htz,ovh,wbx,tsw,lmn,rls --countries=us,de,gb,jp,cn,ca,au
 python3 main.py --blockchain=flow --providers=gcp,aws,htz,ovh --countries=us,de,gb,jp,cn,ca,au
 python3 main.py --blockchain=near --providers=gcp,aws,htz,ovh --countries=us,de,gb,jp,cn,ca,au
