8b0e6049e954e27d9cb517e15e6c8dd8183bb053
Sam <padilla.samuelk@gmail.com>
2023-04-14 16:28:51 +0000

    Updating initial class values for objects

--- classes/dict_initial_values.py
+++ classes/dict_initial_values.py
@@ -1,12 +1,21 @@
 #Initialization dictionaries
 providers_init = {
+    #Other providers
     "Other": {
         "Total Non-Validator Nodes": 0, 
         "Total Validators": 0, 
         "Total Nodes": 0, 
         "Total Stake": 0
         },
+    # Other errors
     "Unidentified": {
+        "Total Non-Validator Nodes": 0,
+        "Total Validators": 0,
+        "Total Nodes": 0,
+        "Total Stake": 0
+    },
+    # Private IP addresses, home, or network masks
+    "Invalid": {
         "Total Non-Validator Nodes": 0,
         "Total Validators": 0,
         "Total Nodes": 0,
@@ -16,7 +25,23 @@ providers_init = {
 
 #Note: There is no other, tracking all continents
 location_init = {
+    # Other errors
     "Unidentified": {
+        "Total Non-Validator Nodes": 0,
+        "Total Validators": 0,
+        "Total Nodes": 0,
+        "Total Stake": 0,
+        "Countries": {
+            "Unidentified": {
+                "Total Non-Validator Nodes": 0,
+                "Total Validators": 0,
+                "Total Nodes": 0,
+                "Total Stake": 0
+                }
+            }
+        },
+    # Private IP addresses, home, or network masks
+    "Invalid": {
         "Total Non-Validator Nodes": 0,
         "Total Validators": 0,
         "Total Nodes": 0,
@@ -53,12 +78,44 @@ providers_init_flow = {
         "Total Stake": {"active": 0, "total": 0},
         "Total Nodes": 0,
         "Total Inactive Nodes": 0
+    },
+    "Invalid": {
+        "Execution Nodes": {"active": 0, "total": 0},
+        "Consensus Nodes": {"active": 0, "total": 0},
+        "Collection Nodes": {"active": 0, "total": 0},
+        "Verification Nodes": {"active": 0, "total": 0},
+        "Access Nodes": {"active": 0, "total": 0},
+        "Total Stake": {"active": 0, "total": 0},
+        "Total Nodes": 0,
+        "Total Inactive Nodes": 0
     }
 }
 
 #Note: There is no other, tracking all continents
 location_init_flow = {
     "Unidentified": {
+        "Execution Nodes": {"active": 0, "total": 0},
+        "Consensus Nodes": {"active": 0, "total": 0},
+        "Collection Nodes": {"active": 0, "total": 0},
+        "Verification Nodes": {"active": 0, "total": 0},
+        "Access Nodes": {"active": 0, "total": 0},
+        "Total Stake": {"active": 0, "total": 0},
+        "Total Nodes": 0,
+        "Total Inactive Nodes": 0,
+        "Countries": {
+            "Unidentified": {
+                "Execution Nodes": {"active": 0, "total": 0},
+                "Consensus Nodes": {"active": 0, "total": 0},
+                "Collection Nodes": {"active": 0, "total": 0},
+                "Verification Nodes": {"active": 0, "total": 0},
+                "Access Nodes": {"active": 0, "total": 0},
+                "Total Stake": {"active": 0, "total": 0},
+                "Total Nodes": 0,
+                "Total Inactive Nodes": 0
+                }
+            }
+        },
+    "Invalid": {
         "Execution Nodes": {"active": 0, "total": 0},
         "Consensus Nodes": {"active": 0, "total": 0},
         "Collection Nodes": {"active": 0, "total": 0},
