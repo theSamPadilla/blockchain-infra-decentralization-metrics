2fe2882db10a21196788c72ecabab2d02a3c8592
Sam <padilla.samuelk@gmail.com>
2023-04-22 03:58:24 +0000

    Adding more providers to AVAX

--- run_all.sh
+++ run_all.sh
@@ -1,4 +1,4 @@
-python3 main.py --blockchain=avalanche --providers=gcp,aws,htz,ovh --countries=us,de,gb,jp,cn,ca,au
+python3 main.py --blockchain=avalanche --providers=gcp,aws,htz,ovh,all,cho --countries=us,de,gb,jp,cn,ca,au
 python3 main.py --blockchain=cardano --providers=gcp,aws,htz,ovh,doc,ctb --countries=us,de,gb,jp,cn,ca,au
 python3 main.py --blockchain=solana --providers=gcp,aws,htz,ovh,wbx,tsw,lmn,rls --countries=us,de,gb,jp,cn,ca,au
 python3 main.py --blockchain=flow --providers=gcp,aws,htz,ovh --countries=us,de,gb,jp,cn,ca,au
