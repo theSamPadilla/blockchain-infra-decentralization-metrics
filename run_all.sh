ecd9933a53a95bde3dba7e278054302ab67a0650
Sam <padilla.samuelk@gmail.com>
2023-05-18 23:21:34 +0000

    Adding aptos command to run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -5,3 +5,4 @@ python3 main.py --blockchain=flow --providers=gcp,aws,htz,ovh --countries=us,de,
 python3 main.py --blockchain=near --providers=gcp,aws,htz,ovh --countries=us,de,gb,jp,cn,ca,au
 python3 main.py --blockchain=ethereum --providers=gcp,aws,htz,ovh --countries=us,de,gb,jp,cn,ca,au
 python3 main.py --blockchain=cosmos --providers=gcp,aws,htz,ovh --countries=us,de,gb,jp,cn,ca,au
+python3 main.py --blockchain=aptos --providers=gcp,aws,htz,ovh --countries=us,de,gb,jp,cn,ca,au
